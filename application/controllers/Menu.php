2dbcf962f07b42f76e17b35c440025937a0ca93d
hikimain <datasai006@gmail.com>
2024-03-12 10:52:44 +0530

    code  type change

--- application/controllers/Menu.php
+++ application/controllers/Menu.php
@@ -21,86 +21,8 @@ class Menu extends CI_Controller {
             redirect('login/index');
         }
     }
-
-    // Add new menu
-
-// public function add() {
-//     $data['menuslevel'] = $this->MenuModel->get_menus_with_level();
-    
-//     if ($this->input->post()) {
-//         $this->load->database();
-//         $this->db->trans_begin();
-        
-//         // Insert menu data and retrieve the inserted ID
-//         $menu_id = $this->MenuModel->add_menu($this->input->post()); 
-        
-//         if ($menu_id) {
-//             // Data successfully inserted, commit the transaction
-//             $this->db->trans_commit();
-            
-//             // Update the menu_id with the inserted ID
-//             $this->MenuModel->update_menu_id($menu_id, $menu_id);
-            
-//             redirect('Menu/add');
-//         } else {
-//             // Error occurred during insertion, rollback the transaction
-//             $this->db->trans_rollback();
-//             echo "Error occurred while adding menu. Please try again.";
-//         }
-//     }
-    
-//     // Fetch menu items and other data
-//      $data['entities'] = $this->MenuModel->get_entities();
-//     $data['menus'] = $this->MenuModel->get_menus();
-//     $data['menu_items'] = $this->MenuModel->get_menu_items();
-    
-//     // Load views
-//     $this->load->view('includes/sidebar', $data);
-//     $this->load->view('menu/add_menu', $data);
-// }
-// public function add() {
-//     $userData = $this->session->userdata('LoginSession');
-//     if (!$userData || !isset($userData['role_id'])) {
-//         // Redirect to login if user is not authenticated or missing role_id in session
-//         redirect('login/index');
-//     }
-
-//     $role_id = $userData['role_id'];
-
-//     if ($this->input->post()) {
-//         // Load necessary libraries and begin transaction
-//         $this->load->database();
-//         $this->db->trans_begin();
-        
-//         // Insert menu data and retrieve the inserted ID
-//         $menu_id = $this->MenuModel->add_menu($this->input->post()); 
-        
-//         if ($menu_id) {
-//             // Data successfully inserted, commit the transaction
-//             $this->db->trans_commit();
-            
-//             // Update the menu_id with the inserted ID
-//             $this->MenuModel->update_menu_id($menu_id, $menu_id);
-            
-//             redirect('Menu/add');
-//         } else {
-//             // Error occurred during insertion, rollback the transaction
-//             $this->db->trans_rollback();
-//             echo "Error occurred while adding menu. Please try again.";
-//         }
-//     }
-    
-//     // Fetch menu items and other data
-//     $data['entities'] = $this->MenuModel->get_entities();
-//     $data['menus'] = $this->MenuModel->get_menus();
-//     $data['menu_items'] = $this->MenuModel->get_menu_items();
-    
-//     // Load views
-//     $this->load->view('includes/sidebar', $data);
-//     $this->load->view('menu/add_menu', $data);
-// }
-
 public function add() {
+     $data['menuslevel'] = $this->MenuModel->get_menus_with_level();
     $userData = $this->session->userdata('LoginSession');
     if (!$userData || !isset($userData['role_id'])) {
         // Redirect to login if user is not authenticated or missing role_id in session
